6c27175abe592bc3b96cf1c8f4c8b58341fffe1b
Sawagi <132923266+JoshuaSawagi@users.noreply.github.com>
2025-06-21 16:22:34 -0500

    Update specials.rs

--- src/fighters/fox/acmd/specials.rs
+++ src/fighters/fox/acmd/specials.rs
@@ -11,7 +11,7 @@ use smash::lib::{L2CValue, L2CAgent};
 //
 use smash::app::*;
 
-unsafe extern "C" fn game_specialairlwstart(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn game_fox_specialairlwstart(agent: &mut L2CAgentBase) {
     frame(agent.lua_state_agent, 1.0);
     if macros::is_excute(agent) {
         macros::ATTACK(agent, 0, 0, Hash40::new("top"), 2.0, 10, 32, 0, 66, 7.5, 0.0, 6.5, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_G, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_elec"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_ELEC, *ATTACK_REGION_ENERGY);
@@ -19,20 +19,20 @@ unsafe extern "C" fn game_specialairlwstart(agent: &mut L2CAgentBase) {
     }
 }
 
-unsafe extern "C" fn effect_specialairlwstart(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn effect_fox_specialairlwstart(agent: &mut L2CAgentBase) {
     if macros::is_excute(agent) {
         macros::EFFECT_FOLLOW(agent, Hash40::new("fox_ref_start"), Hash40::new("top"), 0, 6, 0, 0, 0, 0, 0.75, false);
     }
 }
 
-unsafe extern "C" fn sound_specialairlwstart(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn sound_fox_specialairlwstart(agent: &mut L2CAgentBase) {
     frame(agent.lua_state_agent, 1.0);
     if macros::is_excute(agent) {
         macros::PLAY_SE(agent, Hash40::new("se_fox_special_l01"));
     }
 }
 
-unsafe extern "C" fn expression_specialairlwstart(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn expression_fox_specialairlwstart(agent: &mut L2CAgentBase) {
     if macros::is_excute(agent) {
         slope!(agent, *MA_MSC_CMD_SLOPE_SLOPE, *SLOPE_STATUS_LR);
         ControlModule::set_rumble(agent.module_accessor, Hash40::new("rbkind_shield_on"), 0, false, *BATTLE_OBJECT_ID_INVALID as u32);
@@ -43,7 +43,7 @@ unsafe extern "C" fn expression_specialairlwstart(agent: &mut L2CAgentBase) {
     }
 }
 
-unsafe extern "C" fn game_speciallwstart(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn game_fox_speciallwstart(agent: &mut L2CAgentBase) {
     frame(agent.lua_state_agent, 1.0);
     if macros::is_excute(agent) {
         macros::ATTACK(agent, 0, 0, Hash40::new("top"), 2.0, 10, 32, 0, 66, 7.5, 0.0, 6.5, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_G, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_elec"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_ELEC, *ATTACK_REGION_ENERGY);
@@ -51,7 +51,7 @@ unsafe extern "C" fn game_speciallwstart(agent: &mut L2CAgentBase) {
     }
 }
 
-unsafe extern "C" fn effect_speciallwstart(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn effect_fox_speciallwstart(agent: &mut L2CAgentBase) {
     if macros::is_excute(agent) {
         macros::FOOT_EFFECT(agent, Hash40::new("null"), Hash40::new("top"), 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, false);
         macros::EFFECT_FOLLOW(agent, Hash40::new("fox_ref_flash"), Hash40::new("reflector"), 1.2, 0, -0.5, 0, 0, 0, 1, true);
@@ -62,14 +62,14 @@ unsafe extern "C" fn effect_speciallwstart(agent: &mut L2CAgentBase) {
     }
 }
 
-unsafe extern "C" fn sound_speciallwstart(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn sound_fox_speciallwstart(agent: &mut L2CAgentBase) {
     frame(agent.lua_state_agent, 1.0);
     if macros::is_excute(agent) {
         macros::PLAY_SE(agent, Hash40::new("se_fox_special_l01"));
     }
 }
 
-unsafe extern "C" fn expression_speciallwstart(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn expression_fox_speciallwstart(agent: &mut L2CAgentBase) {
     if macros::is_excute(agent) {
         slope!(agent, *MA_MSC_CMD_SLOPE_SLOPE, *SLOPE_STATUS_LR);
         ControlModule::set_rumble(agent.module_accessor, Hash40::new("rbkind_shield_on"), 0, false, *BATTLE_OBJECT_ID_INVALID as u32);
@@ -78,4 +78,20 @@ unsafe extern "C" fn expression_speciallwstart(agent: &mut L2CAgentBase) {
     if macros::is_excute(agent) {
         macros::RUMBLE_HIT(agent, Hash40::new("rbkind_attacks"), 0);
     }
+}
+
+pub fn install() {
+    Agent::new("fox")
+    .acmd("game_specialairlwstart", game_fox_specialairlwstart, Priority::Low)
+    .acmd("game_speciallwstart", game_fox_speciallwstart, Priority::Low)
+    
+    .acmd("effect_specialairlwstart", effect_fox_specialairlwstart, Priority::Low)
+    .acmd("effect_speciallwstart", effect_fox_speciallwstart, Priority::Low)
+    
+    .acmd("sound_specialairlwstart", sound_fox_specialairlwstart, Priority::Low)
+    .acmd("sound_speciallwstart", sound_fox_speciallwstart, Priority::Low)
+
+    .acmd("expression_specialairlwstart", expression_fox_specialairlwstart, Priority::Low)
+    .acmd("expression_speciallwstart", expression_fox_speciallwstart, Priority::Low)
+    .install();
 }
