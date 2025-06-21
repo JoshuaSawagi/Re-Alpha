36f7caae6528464907220709cbef7c07f20e7817
Sawagi <132923266+JoshuaSawagi@users.noreply.github.com>
2025-06-21 16:18:01 -0500

    Update tilts.rs

--- src/fighters/fox/acmd/tilts.rs
+++ src/fighters/fox/acmd/tilts.rs
@@ -11,8 +11,6 @@ use smash::lib::{L2CValue, L2CAgent};
 //
 use smash::app::*;
 use smash::phx::Vector3f;
-use crate::util::*;
-use super::*;
 
 unsafe extern "C" fn game_fox_attacks3(agent: &mut L2CAgentBase) {
     frame(agent.lua_state_agent, 5.0);
@@ -49,7 +47,7 @@ unsafe extern "C" fn game_fox_attacks3lw(agent: &mut L2CAgentBase) {
         macros::ATTACK(agent, 2, 0, Hash40::new("legr"), 6.0, 361, 80, 0, 40, 3.0, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
         AttackModule::set_attack_height_all(agent.module_accessor, AttackHeight(*ATTACK_HEIGHT_LOW), false);
     }
-    wait(agent.lua_state_agent, 4s.0);
+    wait(agent.lua_state_agent, 4.0);
     if macros::is_excute(agent) {
         AttackModule::clear_all(agent.module_accessor);
     }
